d9d84a84def49970e409bbc2ff4789f092d13503
Darron <darron.haworth@gmail.com>
2014-03-02 03:49:04 +0000

    git-svn-id: https://services.fivepals.com/svn/Fivepals/ResponsiveUnifyTemplate/trunk@432 91f2d19f-b8e2-ee40-8b50-eb9830f26eb7

---
+++ ResponsiveUnify/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResponsiveUnify.MvcApplication" Language="C#" %>
---
+++ ResponsiveUnify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResponsiveUnify.Startup))]
+namespace ResponsiveUnify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
